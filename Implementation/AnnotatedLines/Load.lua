053f7f00dca8643d9368d6572bd2408b6513f752
Ian <ian@somewhere.com>
2024-02-15 07:32:58 +0000

    [!+] Pass #3. Generation detailing, spawned ability to parse results back.

--- Implementation/AnnotatedLines/Load.lua
+++ Implementation/AnnotatedLines/Load.lua
@@ -1,8 +1,42 @@
+-- Parse annotated strings format
+
+--[[
+  No duplicate keys in output. We're keeping the last value.
+
+  Sample input
+
+    a=a 1
+    b=b 1
+    a=a 2
+
+  Sample output
+
+    {
+      a = 'a 2',
+      b = 'b 1',
+    }
+]]
+
+--[[
+  Version: 1
+  Last mod.: 2024-02-15
+]]
+
+local SplitStringToLines = request('!.string.split_to_lines')
+local IterateLines = request('!.string.lines')
+
 return
   function(self, DataString)
     assert_string(DataString)
 
-    local Result = ''
+    local Result = {}
+    for _, Line in IterateLines(DataString) do
+      local Key, Value = ParseLine(Line)
+
+      assert_string(Key)
+      assert_string(Value)
+      Result[Key] = Value
+    end
 
     return Result
   end
