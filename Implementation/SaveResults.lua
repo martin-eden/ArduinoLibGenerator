c7fc7639d8507626c2dae769fb4a893ecc4c0b4c
Ian <ian@somewhere.com>
2024-02-19 10:23:59 +0000

    [.] Pass #6. Smarter use of [file_system.tree].

--- Implementation/SaveResults.lua
+++ Implementation/SaveResults.lua
@@ -1,41 +1,12 @@
 -- Save results. No processing, only file IO business.
 
 --[[
-  Version: 4
-  Last mod.: 2024-02-17
+  Version: 5
+  Last mod.: 2024-02-19
 ]]
 
 local CreateDirTree = request('!.file_system.tree.create')
 local RemoveDirTree = request('!.file_system.tree.remove')
-
-local AddNamePrefix =
-  function(DirTree, Prefix)
-    assert_table(DirTree)
-    assert_string(Prefix)
-
-    --[[
-      We gonna add some directory prefix to string keys in table.
-      Problem that there may already be keys with prefixed name.
-      So we will create prefixed table separately, make original
-      table empty and copy write contents from prefixed table.
-    ]]
-
-    local PrefixedDirTree = {}
-    for Name, Value in pairs(DirTree) do
-      assert_string(Name)
-      local NewName = Prefix .. Name
-      PrefixedDirTree[NewName] = Value
-    end
-
-    -- Make table empty
-    for Key in pairs(DirTree) do
-      DirTree[Key] = nil
-    end
-
-    for Key, Value in pairs(PrefixedDirTree) do
-      DirTree[Key] = Value
-    end
-  end
 
 return
   function(self, DirTree_Create, DirTree_Remove, ResultsDir)
@@ -43,8 +14,9 @@ return
     assert_table(DirTree_Remove)
     assert_string(ResultsDir)
 
-    AddNamePrefix(DirTree_Create, ResultsDir)
-    AddNamePrefix(DirTree_Remove, ResultsDir)
+    -- Apply <ResultsDir> to generated results:
+    DirTree_Create = { [ResultsDir] = DirTree_Create }
+    DirTree_Remove = { [ResultsDir] = DirTree_Remove }
 
     local Problems
     RemoveDirTree(DirTree_Remove)
