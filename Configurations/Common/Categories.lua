61e8ef472deb32030cd956ebd78aefbb4349a6fc
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2024-02-26 18:28:04 +0000

    [!] Pass #8..
    [+] [Configurations.Common]
    [+] [Configurations.me_RoverWilly]

---
+++ Configurations/Common/Categories.lua
@@ -0,0 +1,17 @@
+-- Return table with common ArduinoIDE library categories
+
+-- Last mod.: 2024-02-26
+
+return
+  {
+    Default = 'Uncategorized',
+    Other = 'Other',
+    SignalIO = 'Signal Input/Output',
+    DeviceControl = 'Device Control',
+    Timing = 'Timing',
+    Sensors = 'Sensors',
+    DataProcessing = 'Data Processing',
+    DataStorage = 'Data Storage',
+    Communication = 'Communication',
+    Display = 'Display',
+  }
---
+++ Configurations/me_RoverWilly.lua
@@ -0,0 +1,42 @@
+-- Configuration data for me_RoverWilly library
+
+--[[
+  Version: 1
+  Last mod.: 2024-02-26
+]]
+
+local Categories = request('Configurations.Common.Categories')
+local Architectures = request('Configurations.Common.Architectures')
+
+local Iam =
+  {
+    Name = 'Martin Eden',
+    Email = '',
+  }
+
+return
+  {
+    What =
+      {
+        Name = 'me_RoverWilly',
+        Version = '1.0.0',
+        Category = Categories.DeviceControl,
+        Description = 'Firmware for "Willy" rover. Esplora.',
+        Description_Continued = '',
+        MoreInfo_Url = 'https://github.com/martin-eden/willy',
+      },
+    How =
+      {
+        ReadOnly = true,
+        Architectures = { Architectures.Esp8266 },
+        Dependencies =
+          {
+            { Name = '', VersionExpression = '' },
+          },
+      },
+    Who =
+      {
+        Authors = { Iam },
+        Maintainers = { Iam },
+      }
+  }
