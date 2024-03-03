1457dab3fcda81764162f0df79b6963cda31fdac
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2024-03-03 00:56:35 +0000

    [/] [Categories:] Renamed <.Default> to <.Unset>.

--- Configurations/Common/Categories.lua
+++ Configurations/Common/Categories.lua
@@ -1,11 +1,17 @@
--- Return table with common ArduinoIDE library categories
+-- Return dictionary of common ArduinoIDE library categories
 
--- Last mod.: 2024-02-26
+--[[
+  Category names are hardcoded in format specification.
+  But you can rename fields for your convenience.
+]]
+
+-- Last mod.: 2024-03-02
 
 return
   {
-    Default = 'Uncategorized',
+    Unset = 'Uncategorized',
     Other = 'Other',
+
     SignalIO = 'Signal Input/Output',
     DeviceControl = 'Device Control',
     Timing = 'Timing',
@@ -15,3 +21,8 @@ return
     Communication = 'Communication',
     Display = 'Display',
   }
+
+--[[
+  2024-02-26
+  2024-03-02
+]]
