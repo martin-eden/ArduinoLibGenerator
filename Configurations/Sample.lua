fcfa78cf60dd546a380966d2c061293f9a752d62
Ian <ian@somewhere.com>
2024-03-03 01:21:44 +0000

    [!/] [Configurations.Sample] Sample is no longer simplest possible..
    I decided to make it like my real configs where I use require() to
    load hardcoded lists.

--- Configurations/Sample.lua
+++ Configurations/Sample.lua
@@ -1,8 +1,15 @@
---[[
-  For list of categories and version expressions go to
+-- Sample configuration file
 
-    https://arduino.github.io/arduino-cli/0.35/library-specification/
+--[[
+  We use Lua to return configuration table. This allows us to move
+  constant strings to table and reference them by shorter name.
+
+  Also we are using the same person record <Iam> both in list
+  of authors and maintainers.
 ]]
+
+local Categories = require('Configurations.Common.Categories')
+local Architectures = require('Configurations.Common.Architectures')
 
 local Iam = { Name = 'John Johnson',  Email = '' }
 
@@ -12,14 +19,14 @@ return
       {
         Name = 'MyLibbaName',
         Version = '1.0.0', -- Semver
-        Category = 'Uncategorized',
+        Category = Categories.Unset,
         Description = 'First line of description.',
         Description_Continued = 'Second line of description.',
         MoreInfo_Url = 'https://url-for-more.info',
       },
     How =
       {
-        Architectures = { 'esp8266' },
+        Architectures = { Architectures.Esp8266 },
         Dependencies =
           {
             -- { Name = 'ArduinoJson', VersionExpression = '>=7.0.3' },
