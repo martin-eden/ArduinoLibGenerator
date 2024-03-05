ea8e369e4e5b8838f83a06c2945f7e23e04091cb
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2024-03-05 03:44:45 +0000

    [+] [Results/library.configuration.lua]

---
+++ Results/library.configuration.lua
@@ -0,0 +1,30 @@
+return {
+  How = {
+    Architectures = {
+      '*',
+    },
+    Dependencies = {},
+  },
+  What = {
+    Category = 'Uncategorized',
+    Description = 'Episode IV. A NEW HOPE',
+    Description_Continued = 'It is a period of civil war. Rebel spaceships, striking from a hidden base, have won their first victory against the evil Galactic Empire.',
+    MoreInfo_Url = 'https://url-for-more.info',
+    Name = 'MyLibbaName',
+    Version = '1.0.0',
+  },
+  Who = {
+    Authors = {
+      {
+        Email = '',
+        Name = 'John Johnson',
+      },
+    },
+    Maintainers = {
+      {
+        Email = '',
+        Name = 'John Johnson',
+      },
+    },
+  },
+}
